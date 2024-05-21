b6b97d46684d9ccbe685d9c8221f42cfaa418292
encryptx64 <glasskaxcore@gmail.com>
2024-05-21 17:04:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    window.location.href = 'https://mega.nz/file/s6FQ1DJZ#Q5lnTDkhUCpItldLULcjUmdE8-jok5fJGPHHc1mPSOM';
+});
